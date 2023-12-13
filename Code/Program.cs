c4f48d014f57e74e59b68f8345166353adfee842
alexandr <alexandr_mis93@mail.ru>
2023-12-13 23:08:41 +0500

    добавил строки кода (с 8 по 15)

--- Code/Program.cs
+++ Code/Program.cs
@@ -1,5 +1,15 @@
-﻿Console.Write("Введите элементы массива через пробел: ");
+﻿using System.Linq.Expressions;
+
+Console.Write("Введите элементы массива через пробел: ");
 string[] arr = Console.ReadLine(). Split();
 string[] newArr = new string [arr.Length];
 
 int index = 0;
+foreach(string element int arr)
+{
+    if(ElementInit.Equals Length <= 3)
+    {
+        newArr[index] = element;
+        index++;
+    }
+}
